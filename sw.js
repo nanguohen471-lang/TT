51948375d8a37b5ed4ab05ab1063d0f0e48c7a2a
nanguohen471-lang <nanguohen471@gmail.com>
2025-09-18 04:58:03 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('tetris-neo-v4').then(c=>c.addAll([
-    './index.html','./manifest.json','./icon.png'
-  ])));
-});
-self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
