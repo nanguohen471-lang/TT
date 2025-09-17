933e7fac8bdb009370bd8c29903f91547bf52652
nanguohen471-lang <nanguohen471@gmail.com>
2025-09-18 04:24:47 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('tetris-neo-v1').then(c=>c.addAll([
+    './index.html','./manifest.json','./icon.png',
+    './bgm.mp3','./rotate.mp3','./clear.mp3','./tspin.mp3','./gameover.mp3'
+  ])));
+});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
